842fa694c2d006056a87769722616f4b4ceac2ec
Apoorv <vapoorve@gmail.com>
2019-03-04 13:22:11 +0530

    Hellow World code

--- [1] hellow world/write()_and_end().js
+++ [1] hellow world/write()_and_end().js
@@ -3,18 +3,7 @@ var http = require('http');
 
 http.createServer(
     function (req,res) {
-        var data  = `<!DOCTYPE html>
-<html>
-<head>
-    <title>Apoorve</title>
-</head>
-<body>
-<h1>Apoorve</h1>
-<p>my name is Apoorve kumar Verma</p>
-</body>
-</html>`;
-       res.write(data);
-       res.write("test String");
+        res.write("Hellow World");
         res.end("('+')");
     }
 ).listen(8080);
