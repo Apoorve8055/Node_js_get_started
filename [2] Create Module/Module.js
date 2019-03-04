85ff2ef53882bb49b86da9be13df21f715b78e74
Apoorv <vapoorve@gmail.com>
2019-03-04 14:32:53 +0530

    Create Module

--- [2] Create Module/Module.js
+++ [2] Create Module/Module.js
@@ -2,3 +2,12 @@ exports.hw = 'Hello Apoorve';
 exports.date = function () {
     return Date();
 }
+    var math = {
+        add:function (a,b) {
+            return String( a+b);
+        },
+        multi:function (a,b){
+            return String(a*b);
+        }
+    }
+    exports.fun = math;
--- [2] Create Module/app.js
+++ [2] Create Module/app.js
@@ -2,10 +2,11 @@
 var http = require('http');
 var mod = require('./Module.js');
 http.createServer(function (req,res) {
-    res.write(mod.hw +" Date :" + mod.date());
+    res.write(" "+mod.hw +" \n Date :" + mod.date());
+    res.write("\n Add : "+ mod.fun.add(2,3) +"\n Multi : "+mod.fun.multi(2,3));
     res.end();
 }).listen(8080);
 
 // on console
 
-console.log(mod.hw," Date :",mod.date());
+console.log(mod.hw," Date :",mod.date(),mod.fun.add(2,3),mod.fun.multi(2,3));
