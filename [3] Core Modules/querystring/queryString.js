4ae0968b0b6acac3ed24dd03e194eeb412e9e5b7
Apoorv <vapoorve@gmail.com>
2019-03-06 15:03:42 +0530

    Query String Module

---
+++ [3] Core Modules/querystring/queryString.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var url = require('url');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var q = url.parse(req.url, true).query;
+    res.write(q.name + q.phone)
+    res.end();
+}).listen(8080);
