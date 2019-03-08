037fc3468a821aa7323c92cad53dc6c8a40911cf
Apoorv <vapoorve@gmail.com>
2019-03-08 17:53:08 +0530

    Serving Static File in Express Freamwork using nodejs

--- [11] Serving Static Files/server.js
+++ [11] Serving Static Files/server.js
@@ -1,16 +1,15 @@
 var express = require('express');
-var path = require('path');
-var app = express();
+var app =  express();
+
+app.set('view engine','pug');
+app.set('views','./views');
 
 app.use(express.static('public'));
+app.get('/home',function(req,res){
+    res.render('index',{
+        name:"Apoorve"
+    });
 
-app.get('/', function (req, res) {
-    res.send('Hello World');
-})
+});
 
-var server = app.listen(8081, function () {
-    var host = server.address().address
-    var port = server.address().port
-
-    console.log("Example app listening at http://%s:%s", host, port)
-})
+app.listen(8080);
