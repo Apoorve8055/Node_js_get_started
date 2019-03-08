f8ee26acaf7bc7ef9f19e5a58919976394d2f623
Apoorv <vapoorve@gmail.com>
2019-03-08 17:53:55 +0530

    Post Data & Form Submission in Express Freamwork using nodejs

---
+++ [15]  Post Data/app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
+var app = express();
+
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+// for parsing application/json
+app.use(bodyParser.json());
+
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true }));
+//form-urlencoded
+
+// for parsing multipart/form-data
+app.use(upload.array());
+
+app.post('/', function(req, res){
+    console.log(req.body);
+    res.send("recieved your request!");
+});
+app.listen(8080);
