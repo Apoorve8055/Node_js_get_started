f246a3f0a39b3f45fcb04bc3a301c86d0de850c4
Apoorv <vapoorve@gmail.com>
2019-03-11 18:43:28 +0530

    Connect MonogoDb with Mongoose in Express Freamwork using nodejs

---
+++ [16] Database(mongodb)/app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var assert = require('assert');
+var app = express();
+
+var url = "mongodb://localhost:27017/mydb";
+mongoose.connect(url);
+
+var db = mongoose.connection;
+
+db.on('error',console.error.bind(console,"connection error"));
+
+db.on('open',function(){
+   console.log('DB Connection Successful!');
+});
+
+
+mongoose.model('tut',{topic: String });
+
+app.get('/',function(req,res){
+
+   mongoose.model('tut').find({'name':'Apoorve'},{_id:0},function(err,tut){
+      res.send("<pre>"+tut+"<pre>");
+   });
+
+});
+
+app.listen(8080,function(){
+   console.log('connected');
+});
