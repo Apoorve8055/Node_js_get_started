f41c9d28fbed0362e6d2c51f5d0afcc9f7691d3d
Apoorv <vapoorve@gmail.com>
2019-03-12 18:17:20 +0530

    Session handling in Express Freamwork using nodejs

--- [19] Session/app.js
+++ [19] Session/app.js
@@ -1,15 +1,21 @@
 var express = require('express');
 var session = require('express-session');
 var app = express();
-app.use(session({secret: "Shh, its a secret!"}));
+app.use(session({secret: "Session encription"}));
 
 app.get('/', function(req, res){
     if(req.session.viewCount){
         req.session.viewCount++;
-        res.send('Page View : '+req.session.viewCount);
+        res.send('Page View : '+req.session.viewCount+'<br> <a href="/destroy">Destroy Session </a>');
+
     }else{
         req.session.viewCount = 1;
         res.send('Welcome to this Session Page use count the page view using Session <br>'+'Page View : '+req.session.viewCount);
     }
 });
+
+app.get('/destroy', function(req, res){
+        req.session.destroy();
+});
+
 app.listen(8080);
