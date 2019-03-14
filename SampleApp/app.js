90a6b4bacc6c08542f07db433244b2fba7766431
Apoorv <vapoorve@gmail.com>
2019-03-14 21:45:02 +0530

    SampleWebApp in Express Freamwork using nodejs

--- SampleApp/app.js
+++ SampleApp/app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
-
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : true}));
 app.set('view engine','ejs');
 
 var data = {
@@ -20,6 +22,20 @@ app.get('/About',function(req,res){
         fname: data.fname,
         lname: data.lname
     });
+});
+
+app.get('/Login',function(req,res){
+    res.render('login',{auth:0});
+});
+
+app.post('/loginAuth',function(req,res){
+    var user = req.body.user;
+    var pass = req.body.pass;
+    if(user == "Apoorve" && pass == "12345"){
+       res.render('login',{auth:1} );
+    }else{
+        res.render('login',{auth:2} );
+    }
 });
 
 app.listen(8080);
