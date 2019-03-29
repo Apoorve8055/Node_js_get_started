dfd7bb59bc1f0aa9223f5cb573af5d5e1ab16a75
Apoorv <vapoorve@gmail.com>
2019-03-29 17:16:58 +0530

    Mysql in Express Freamwork using nodejs

--- [24] Mysql/app.js
+++ [24] Mysql/app.js
@@ -8,7 +8,8 @@ var con = mysql.createConnection({
     password: "",
     database: "node_db"
 });
-var qdata;
+
+
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
@@ -17,26 +18,50 @@ con.connect(function(err) {
         if (err) throw err;
         console.log("Database created");
     });
-
-
-con.query("CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), pass VARCHAR(255))",function(err,result){
-    if(err) throw err;
-    console.log("Table Created :"+result);
-});
-
-con.query("insert into users set name = 'Apoorve' , pass = '12345' ",function(err,result){
-        if(err) throw err;
-        console.log("Table Created :"+result);
-});
-
-    con.query("SELECT * FROM USERS", function (err, result, fields) {
-        if (err) throw err;
-        console.table(result);
-        console.log(result);
-    });
 */
 
+    function Show(){
+        con.query("SELECT * FROM USERS", function (err, result, fields) {
+            if (err) throw err;
+            console.table(result);
+            console.log(result);
+        });
+    }
+
+
+    con.query("CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), pass VARCHAR(255))",function(err,result){
+        if(err) throw err;
+        console.log("Table Created :"+result);
+
+    });
+
+    Show();
+
+    con.query("insert into users set name = 'Apoorve' , pass = '12345' ",function(err,result){
+            if(err) throw err;
+            console.log("Table Created :"+result);
+    });
+    con.query("insert into users set name = 'Avi' , pass = '12345' ",function(err,result){
+        if(err) throw err;
+        console.log("Table Created :"+result);
+    });
+
+    Show();
+
+
+    con.query('update users set name ="Avi" where id = 2',function(err,result,fields){
+        console.log("Update Data");
+    });
+    Show();
+
+    con.query('delete from users where id=2 ',function(err,result,fields){
+        console.log("Delete Data");
+    });
+
+    Show();
+
 });
+
 
 
 app.listen(8080);
